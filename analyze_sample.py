01ba39c6ba71c7b015f82e79cd7545146485f340
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-07-16 14:30:51 -0600

    created first draft of database access class

--- analyze_sample.py
+++ analyze_sample.py
@@ -1,4 +1,3 @@
-import vcf
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,104 +5,146 @@ from sys import argv
 
 """
 Parameters
-1. path to g.vcf file
-2. path to output dir (needs to already exist and should be unique for the sample)
+1. path to pileup file
+2. path to vcf file
+3. path to output dir (needs to already exist and should be unique for the sample)
 """
-# argv = ['', '/Users/michael/TESTBAMs/HaplotypeCaller_GIABO1_S53.g.vcf', './']
-# make sure the output dir ends with a '/'
-output_dir = argv[2]
-if output_dir[-1] != '/':
-    output_dir += '/'
 
-vcf_reader = vcf.Reader(open(argv[1], 'r'))
 
-summary_stats = {'chromosome': [],
-                 'position': [],
-                 'genotype': [],
-                 'diff': [],
-                 'allele_balance': [],
-                 'geno_string': [],
-                 'number_of_alleles': []}
-for i, record in enumerate(vcf_reader):
-    if i % 1000 == 0:
-        print(i)
-    # check if the record has allelic depth information we need this for stats
-    if 'AD' in record.samples[0].data._asdict():
-        # get the genotype
-        gt = record.samples[0]['GT']
-        # if all allele depths are 0, skip this location
-        if sum(record.samples[0]['AD']) == 0:
+def collect_and_output_genotypes(input_file, output_file, coverage_threshold, geno_dict):
+    ab_info = {'chromosome': [],
+               'position': [],
+               'genotype': [],
+               'alleles': [],
+               'allele_counts': [],
+               'allele_balance': [],
+               'num_alleles': [],
+               'first_allele': [],
+               'reference_allele': []}
+    count = 0
+    geno_count = 0
+    geno_count_unknown = 0
+    for line in open(input_file, 'r'):
+        if count % 10000 == 0:
+            pass
+            # print(count)
+        count += 1
+        row = line.split('\t')
+
+        if row[0] != '1':
+            break
+
+        ref_allele = row[2]
+        seq = row[4]
+        # if there if not enough coverage, skip it
+        if len(seq) < coverage_threshold:
             continue
-        # calc allele balance of reference allele / total # of alleles
-        ab = record.samples[0]['AD'][0] / sum(record.samples[0]['AD'])
-        # variable to store how much this site differs from the expected
-        diff = -1
-        # variable for storing the simple genotype (hetero or homo)
-        geno_string = 'homo'
-        # check if it is homozygous
-        if re.search(r'(\d)\/\1', gt):
-            # is it homozygous reference or not? if it is homozygous, expected allele balance is 1.0
-            if gt[0] == '0':
-                # is reference
-                diff = 1.0 - ab
-            else:
-                # is alternate
-                diff = 0.0 - ab
+
+        # remove indel information about bases to follow
+        seq = re.sub('\\+[0-9]+[ACGTNacgtn]', '', seq)
+        seq = re.sub('-[0-9]+[ACGTNacgtn]', '', seq)
+
+        seq = seq.upper()
+        seq = seq.replace(',', ref_allele)
+        seq = seq.replace('.', ref_allele)
+
+        # remove > and < as these represent skips to the reference genome
+        seq = seq.replace('>', '').replace('<', '')
+        # remove * which denote gap in the read
+        seq = seq.replace('*', '')
+        # remove $ which denote end of a read
+        seq = seq.replace('$', '')
+        # remove beginning of read marker and the quality character that follows
+        seq = re.sub('\\^.', '', seq)
+
+        # count of the occurrence of each allele
+        seq_list = [char for char in seq]
+        alleles = list(set(seq_list))
+        allele_counts = [seq_list.count(x) for x in alleles]
+
+        # sort alleles alphabetically
+        alleles, allele_counts = zip(*sorted(zip(alleles, allele_counts)))
+
+        # get the genotype
+        genotype = None
+        if str(row[1]) in geno_dict[str(row[0])]:
+            genotype = geno_dict[str(row[0])][str(row[1])]
+            geno_count += 1
         else:
-            geno_string = 'hetero'
-            # if it is heterozygous, expected allele balance is 0.5
-            diff = abs(0.5 - ab)
-        summary_stats['chromosome'].append(record.CHROM)
-        summary_stats['position'].append(record.POS)
-        summary_stats['genotype'].append(gt)
-        summary_stats['allele_balance'].append(ab)
-        summary_stats['diff'].append(diff)
-        summary_stats['geno_string'].append(geno_string)
-        # the number of alleles is the number of non-zero allele counts
-        summary_stats['number_of_alleles'].append(len(record.samples[0]['AD']) - record.samples[0]['AD'].count(0))
-    else:
-        # there is no allelic depth information so skip it
-        pass
+            genotype = row[2] + '/' + row[2]
+            geno_count_unknown += 1
 
-df = pd.DataFrame(summary_stats)
+        # calc AB as mode count / all count
+        first_allele = genotype.split('/')[0]
+        print()
+        print(alleles)
+        print(first_allele)
+        index = alleles.index(first_allele)
 
-extreme = sum(df['diff'] >= 0.2)
-extreme_hetero = sum(df[df['geno_string'] == 'hetero']['diff'] >= 0.2)
-extreme_homo = sum(df[df['geno_string'] == 'homo']['diff'] >= 0.2)
-moderate = sum(df['diff'] >= 0.05)
-moderate_hetero = sum(df[df['geno_string'] == 'hetero']['diff'] >= 0.05)
-moderate_homo = sum(df[df['geno_string'] == 'homo']['diff'] >= 0.05)
+        ab = allele_counts[index] / sum(allele_counts)
+        ab_info['chromosome'].append(row[0])
+        ab_info['position'].append(row[1])
+        ab_info['reference_allele'].append(row[3])
+        ab_info['allele_balance'].append(ab)
+        ab_info['num_alleles'].append(len(alleles))
+        ab_info['first_allele'].append(alleles[index])
+        # genotype will be formatted like this: 'A/C' or 'T/T'
+        ab_info['genotype'].append(genotype)
+        ab_info['alleles'].append('/'.join(alleles))
+        ab_info['allele_counts'].append(','.join([str(x) for x in allele_counts]))
 
-with open(output_dir + 'allelic_balance_counts.txt', 'w') as file:
-    file.write('Percent heterozygous: ' + str(sum(df['geno_string'] == 'hetero') / df.shape[0]) + '\n')
-    file.write('Heterozygous count: ' + str(sum(df['geno_string'] == 'hetero')) + '\n')
-    file.write('Homozygous count: ' + str(sum(df['geno_string'] == 'homo')) + '\n')
-    file.write('Number of extreme differences: ' + str(extreme) + '\n')
-    file.write('Number of extreme heterozygous differences: ' + str(extreme_hetero) + '\n')
-    file.write('Number of extreme homo differences: ' + str(extreme_homo) + '\n')
-    file.write('Number of moderate differences: ' + str(moderate) + '\n')
-    file.write('Number of moderate heterozygous differences: ' + str(moderate_hetero) + '\n')
-    file.write('Number of moderate homo differences: ' + str(moderate_homo) + '\n')
+    pd.DataFrame(ab_info).to_csv(output_file, sep='\t')
 
-# plot allelic depth
-plt.hist(
-    [df[df['geno_string'] != 'hetero']["allele_balance"], df[df['geno_string'] == 'hetero']["allele_balance"]],
-    label=['Homozygous', 'Heterozygous'], bins=[x / 100 for x in range(0, 101, 2)])
-plt.legend(loc='upper right')
-plt.xlabel('Allele balance')
-plt.ylabel('Count')
-plt.savefig(output_dir + 'allele_balance.png')
-plt.clf()
 
-# plot difference of expected vs observed
-plt.hist(
-    [df[df['geno_string'] != 'hetero']["diff"], df[df['geno_string'] == 'hetero']["diff"]],
-    label=['Homozygous', 'Heterozygous'], bins=[x / 100 for x in range(0, 101, 2)])
-plt.legend(loc='upper right')
-plt.xlabel('Difference from expected')
-plt.ylabel('Count')
-plt.savefig(output_dir + 'difference.png')
-plt.clf()
+# collect_and_output_genotypes(argv[1], argv[3], 40)
 
-# save summary stats df to a file
-df.to_csv(output_dir + 'summary_stats.tsv', sep='\t')
+def get_genotype_dict_from_vcf(vcf: str) -> dict:
+    """
+    Create a dictionary of the genotype in the given vcf file. Dictionary is formated as:
+    a dictionary of dictionaries with chromosome (string) as the first key and position as the second
+    The genotype is sorted alphabetically for example 'A/T' or 'C/G' and NEVER 'T/A' or 'G/C'
+    :param vcf: path to vcf file
+    :return: dictionary of genotypes
+    """
+    # dictionary of dictionaries with chromosome as the first key and position as the second
+    geno_dict = {}
+    for line in open(vcf, 'r'):
+        # skip headers
+        if line[:1] == '#':
+            continue
+        row = line.split('\t')
+        ref = row[3]
+        alt = row[4]
+        chrom = row[0]
+        pos = row[1]
+        numerical_genotype = row[9].split(':')[0]
+        numerical_genotype = numerical_genotype.replace('|', '/')
+        genotype = ''
+        if numerical_genotype == '1/1':
+            genotype = alt + '/' + alt
+        elif numerical_genotype == '0/1':
+            genotype = ref + '/' + alt
+        elif numerical_genotype == '1/2':
+            alleles = row[4].split(',')
+            # reverse soring so they get joined in the right order
+            alleles.sort(reverse=True)
+            genotype = '/'.join(alleles)
+        else:
+            print(numerical_genotype)
+            print('ERROR')
+            genotype = 'ERROR'
+        # if a the chromosome has not been seen before, create a new entry for it
+        if chrom not in geno_dict:
+            geno_dict[chrom] = {}
+        # add the genotype in for the chromosome and position
+        geno_dict[chrom][pos] = genotype
+    return geno_dict
+
+
+gd = get_genotype_dict_from_vcf(argv[2])
+collect_and_output_genotypes(argv[1], argv[3], 40, gd)
+# TODO use first allele in genotype for allele balance calculations
+# TODO calc z score
+# TODO add strand bias to this script
+# TODO strand bias DB creation
+# TODO make a summary report
---
+++ database_access.py
@@ -0,0 +1,129 @@
+import sqlite3
+import pandas as pd
+import numpy as np
+
+
+class DatabaseAccess:
+    def __init__(self, db_path):
+        self.connection = sqlite3.connect(db_path)
+        create_chromosome_table_template = """ CREATE TABLE IF NOT EXISTS {table}
+        (
+        position INTEGER PRIMARY KEY,
+        genotype TEXT,
+        alleles TEXT,
+        allele_counts TEXT,
+        allele_balances TEXT,
+        mode_alleles TEXT,
+        reference_allele TEXT
+        );
+        """
+        # create a table for each chromosome. the names for tables with be chromosome_1, chromosome_x etc...
+        chromosomes = [str(x) for x in range(1, 24)]
+        chromosomes.append('x')
+        chromosomes.append('y')
+        curs = self.connection.cursor()
+        for c in chromosomes:
+            statement = create_chromosome_table_template.format(table='chromosome_' + c)
+            print(statement)
+            a = curs.execute(statement)
+
+    def print_tables(self):
+        curs = self.connection.cursor()
+        curs.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print(curs.fetchall())
+
+    def update_db_with_tsv(self, path_to_tsv: str):
+        """
+        Given a tsv, add its information into the database
+        :param path_to_tsv: path to a tsv (typically the output of analyze_sample.py) with these columns:
+            chromosome	position	genotype	diff	allele_balance	geno_string	number_of_alleles
+        :return: None
+        """
+        df = pd.read_csv(path_to_tsv, sep='\t')
+        get_template = """SELECT allele_balances, allele_counts, alleles, mode_alleles 
+        FROM chromosome_{chrom} WHERE position = {pos} AND genotype = "{genotype}"
+        """
+        update_template = """ UPDATE chromosome_{chrom}
+        SET 
+        allele_balances = "{ab}",
+        allele_counts = "{ac}",
+        alleles = "{alleles}",
+        mode_alleles = "{ma}"
+        WHERE
+        position = {pos}
+        AND
+        genotype = "{genotype}"
+        """
+        insert_template = """INSERT INTO chromosome_{chrom} 
+        (position, allele_balances, allele_counts, genotype, mode_alleles, alleles)
+        VALUES ({pos},"{ab}","{ac}","{genotype}","{ma}","{alleles}")
+        """
+        # for each chromosome
+        for c in set(df['chromosome']):
+            sub = df[df['chromosome'] == c]
+            sub.index = sub['position']
+            # for each position
+            curs = self.connection.cursor()
+            for p in sub['position']:
+                allele_balance = sub.loc[p]['allele_balance']
+                mode_allele = sub.loc[p]['mode_allele']
+                allele_count = sub.loc[p]['allele_counts']
+                genotype = sub.loc[p]['genotype']
+                # TODO when genotype calling is added this will no longer be the same information
+                alleles = sub.loc[p]['genotype']
+                curs.execute(get_template.format(chrom=c, pos=p, genotype=genotype))
+                res = curs.fetchall()
+                if len(res) == 0:
+                    print('Nada')
+                    # there is no information at this position and genotype
+                    # use the insert template
+                    statement = insert_template.format(chrom=c, pos=p, ab=str(allele_balance),
+                                                       ac=str(allele_count), genotype=genotype, ma=mode_allele,
+                                                       alleles=alleles)
+                    curs.execute(statement)
+                else:
+                    # use the update template
+                    statement = update_template.format(chrom=c,
+                                                       pos=p,
+                                                       ab=res[0][0] + ';' + str(allele_balance),
+                                                       ac=res[0][1] + ';' + str(allele_count),
+                                                       genotype=genotype,
+                                                       ma=res[0][2] + ';' + mode_allele,
+                                                       alleles=res[0][3] + ';' + alleles)
+                    curs.execute(statement)
+            # commit changes after each chromosome
+            self.connection.commit()
+
+    def get_mean_and_std_dev(self, chromosome: str, position: int, genotype: str) -> [float, float]:
+        """
+        Get the mean and standard deviation of allele balance at a certain position
+        :param chromosome: string, single character chromosome '1','2' ... 'x', 'y'
+        :param position: integer, numerical position on the chromosome
+        :param genotype: string, genotype of interest
+        :return: list, [allele balance mean, allele balance standard deviation] or
+                 [-1,-1] if there is no information about the desired location
+        """
+        get_template = """SELECT allele_balances, allele_counts, alleles, mode_alleles 
+        FROM chromosome_{chrom} WHERE position = {pos} AND genotype = "{genotype}"
+        """
+        curs = self.connection.cursor()
+        statement = get_template.format(chrom=chromosome, pos=position, genotype=genotype)
+        curs.execute(statement)
+        res = curs.fetchall()
+        # allele balance is the first item returned from the query
+        # here I assume there is only 1 entry that matches the query
+        ab = res[0][0]
+        print(ab)
+        # split up the string into individual allele balances (';' denotes separation between samples)
+        allele_balances = [float(x) for x in ab.split(';')]
+        mean = sum(allele_balances) / len(allele_balances)
+        std = np.std(allele_balances)
+        return [mean, std]
+
+
+
+da = DatabaseAccess('ab.db')
+# da.update_db_with_tsv('really_small.tsv')
+print(da.get_mean_and_std_dev('1', 14747, 'C/C'))
+# da.print_tables()
+# da.get_allele_balance_population_mean_and_std_dev('1', 0)

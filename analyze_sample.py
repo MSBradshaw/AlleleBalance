e4199ed48d7977c452e07a1f2558c9a6fa7fb0ad
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2020-08-03 08:37:10 -0600

    combine the analyze code and database access code into a single file to be used as a command line tool

--- analyze_sample.py
+++ analyze_sample.py
@@ -1,17 +1,132 @@
 import re
 import pandas as pd
 from sys import argv
-from database_access import DatabaseAccess
-
-"""
-Parameters
-1. path to pileup file
-2. path to vcf file
-3. path to output dir (needs to already exist and should be unique for the sample)
-"""
+# from database_access import DatabaseAccess
+import sqlite3
+import numpy as np
+import argparse
 
 
-def collect_and_output_genotypes(input_file, output_file, coverage_threshold, geno_dict):
+class DatabaseAccess:
+    def __init__(self, db_path):
+        self.connection = sqlite3.connect(db_path)
+        create_chromosome_table_template = """ CREATE TABLE IF NOT EXISTS {table}
+        (
+        position INTEGER PRIMARY KEY,
+        genotype TEXT,
+        alleles TEXT,
+        allele_counts TEXT,
+        allele_balances TEXT,
+        first_alleles TEXT,
+        reference_allele TEXT
+        );
+        """
+        # create a table for each chromosome. the names for tables with be chromosome_1, chromosome_x etc...
+        chromosomes = [str(x) for x in range(1, 24)]
+        chromosomes.append('x')
+        chromosomes.append('y')
+        curs = self.connection.cursor()
+        for c in chromosomes:
+            statement = create_chromosome_table_template.format(table='chromosome_' + c)
+            print(statement)
+            log_file.write(statement+'\n')
+            a = curs.execute(statement)
+
+    def print_tables(self):
+        curs = self.connection.cursor()
+        curs.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print(curs.fetchall())
+
+    def update_db_with_tsv(self, path_to_tsv: str):
+        """
+        Given a tsv, add its information into the database
+        :param path_to_tsv: path to a tsv (typically the output of analyze_sample.py) with these columns:
+            chromosome	position	genotype	diff	allele_balance	geno_string	number_of_alleles
+        :return: None
+        """
+        df = pd.read_csv(path_to_tsv, sep='\t')
+        get_template = """SELECT allele_balances, allele_counts, alleles, first_alleles 
+        FROM chromosome_{chrom} WHERE position = {pos} AND genotype = "{genotype}"
+        """
+        update_template = """ UPDATE chromosome_{chrom}
+        SET 
+        allele_balances = "{ab}",
+        allele_counts = "{ac}",
+        alleles = "{alleles}",
+        first_alleles = "{ma}"
+        WHERE
+        position = {pos}
+        AND
+        genotype = "{genotype}"
+        """
+        insert_template = """INSERT INTO chromosome_{chrom} 
+        (position, allele_balances, allele_counts, genotype, first_alleles, alleles)
+        VALUES ({pos},"{ab}","{ac}","{genotype}","{ma}","{alleles}")
+        """
+        # for each chromosome
+        for c in set(df['chromosome']):
+            sub = df[df['chromosome'] == c]
+            sub.index = sub['position']
+            # for each position
+            curs = self.connection.cursor()
+            for p in sub['position']:
+                allele_balance = sub.loc[p]['allele_balance']
+                first_alleles = sub.loc[p]['first_allele']
+                allele_count = sub.loc[p]['allele_counts']
+                genotype = sub.loc[p]['genotype']
+                alleles = sub.loc[p]['alleles']
+                curs.execute(get_template.format(chrom=c, pos=p, genotype=genotype))
+                res = curs.fetchall()
+                if len(res) == 0:
+                    # there is no information at this position and genotype
+                    # use the insert template
+                    statement = insert_template.format(chrom=c, pos=p, ab=str(allele_balance),
+                                                       ac=str(allele_count), genotype=genotype, ma=first_alleles,
+                                                       alleles=alleles)
+                    curs.execute(statement)
+                else:
+                    # use the update template
+                    statement = update_template.format(chrom=c,
+                                                       pos=p,
+                                                       ab=res[0][0] + ';' + str(allele_balance),
+                                                       ac=res[0][1] + ';' + str(allele_count),
+                                                       genotype=genotype,
+                                                       ma=res[0][2] + ';' + first_alleles,
+                                                       alleles=res[0][3] + ';' + alleles)
+                    curs.execute(statement)
+            # commit changes after each chromosome
+            self.connection.commit()
+
+    def get_mean_and_std_dev(self, chromosome: str, position: int, genotype: str) -> [float, float]:
+        """
+        Get the mean and standard deviation of allele balance at a certain position
+        :param chromosome: string, single character chromosome '1','2' ... 'x', 'y'
+        :param position: integer, numerical position on the chromosome
+        :param genotype: string, genotype of interest
+        :return: list, [allele balance mean, allele balance standard deviation] or
+                 [-1,-1] if there is no information about the desired location
+        """
+        get_template = """SELECT allele_balances, allele_counts, alleles, first_alleles 
+        FROM chromosome_{chrom} WHERE position = {pos} AND genotype = "{genotype}"
+        """
+        curs = self.connection.cursor()
+        statement = get_template.format(chrom=chromosome, pos=position, genotype=genotype)
+        curs.execute(statement)
+        res = curs.fetchall()
+        # if there are no results, return [-1, -1]
+        if len(res) == 0:
+            return [-1, -1]
+        # allele balance is the first item returned from the query
+        # here I assume there is only 1 entry that matches the query
+        ab = res[0][0]
+        # split up the string into individual allele balances (';' denotes separation between samples)
+        allele_balances = [float(x) for x in ab.split(';')]
+        mean = sum(allele_balances) / len(allele_balances)
+        std = np.std(allele_balances)
+        return [mean, std]
+
+
+def collect_and_output_genotypes(input_file, output_file, db_path, coverage_threshold, geno_dict):
     ab_info = {'chromosome': [],
                'position': [],
                'genotype': [],
@@ -23,15 +138,12 @@ def collect_and_output_genotypes(input_file, output_file, coverage_threshold, ge
                'reference_allele': [],
                'z_score': [],
                'strand_bias': []}
-    da = DatabaseAccess('ab.db')
+    da = DatabaseAccess(db_path)
     count = 0
     geno_count = 0
     geno_count_unknown = 0
     not_enough_count = 0
     for line in open(input_file, 'r'):
-        if count % 10000 == 0:
-            # pass
-            print(count)
         count += 1
         row = line.split('\t')
 
@@ -86,10 +198,10 @@ def collect_and_output_genotypes(input_file, output_file, coverage_threshold, ge
         try:
             index = alleles.index(first_allele)
         except ValueError:
-            print('Error: genotype not found in alleles')
-            print(first_allele)
-            print(row)
-            print()
+            log_file.write('Error: genotype not found in alleles\n')
+            log_file.write(str(first_allele)+'\n')
+            log_file.write(str(row) + '\n\n')
+            log_file.write('Error: genotype not found in alleles\n')
 
         # calculate allele balance
         ab = allele_counts[index] / sum(allele_counts)
@@ -99,7 +211,7 @@ def collect_and_output_genotypes(input_file, output_file, coverage_threshold, ge
         z_score = -1
         if -1 in mean_std_arr:
             not_enough_count += 1
-            print('Not enought info in DB to calculate Z-score')
+            log_file.write('Not enough info in DB to calculate Z-score\n')
         else:
             # caluclate the z-score, z = (x - mean) / standard deviation
             z_score = (ab - mean_std_arr[0]) / mean_std_arr[1]
@@ -116,12 +228,17 @@ def collect_and_output_genotypes(input_file, output_file, coverage_threshold, ge
         ab_info['allele_counts'].append(','.join([str(x) for x in allele_counts]))
         ab_info['z_score'].append(z_score)
         ab_info['strand_bias'].append(strand_bias)
-        print(strand_bias)
 
     df = pd.DataFrame(ab_info)
     df.to_csv(output_file, sep='\t')
-    print(not_enough_count)
-    print(df.shape)
+
+    out_of_range_count = sum(df['z_score'] >= 3) + sum(df['z_score'] <= -3)
+    with open(output_file, 'w') as file:
+        file.write('# number of samples out of range:\t' + str(out_of_range_count))
+    df.to_csv(output_file, mode='a', header=True)
+
+    return df
+
 
 def get_genotype_dict_from_vcf(vcf: str) -> dict:
     """
@@ -156,8 +273,10 @@ def get_genotype_dict_from_vcf(vcf: str) -> dict:
             alleles.sort(reverse=True)
             genotype = '/'.join(alleles)
         else:
-            print(numerical_genotype)
+            log_file.write('Unknown genotype in get_genotype_dict_from_vcf:\t')
+            log_file.write(str(numerical_genotype) + '\n')
             print('ERROR')
+            print(numerical_genotype)
             genotype = 'ERROR'
         # if a the chromosome has not been seen before, create a new entry for it
         if chrom not in geno_dict:
@@ -167,7 +286,75 @@ def get_genotype_dict_from_vcf(vcf: str) -> dict:
     return geno_dict
 
 
-gd = get_genotype_dict_from_vcf(argv[2])
-collect_and_output_genotypes(argv[1], argv[3], 40, gd)
+def run_sample(db_path):
+    """
+       Required Files
+       1. sample.pileup
+       2. sample.vcf
+       3. Path to DB, as sys.argv[1]
+       Output
+       1. ab_report.txt
+       """
 
-# TODO make a summary report
+    pileup_file = '/Users/michael/TESTBAMs/WES10_S21.pileup'
+    vcf_file = '/Users/michael/TESTBAMs/NexteraCap-Twist-110465_S35_L001.vcf'
+    output_file = 'output.tsv'
+
+    gd = get_genotype_dict_from_vcf(vcf_file)
+    df = collect_and_output_genotypes(pileup_file, output_file, db_path,40, gd)
+
+    out_of_range_count = sum(df['z_score'] >= 3) + sum(df['z_score'] <= -3)
+    with open(output_file, 'w') as file:
+        file.write('# number of samples out of range:\t' + str(out_of_range_count))
+    df.to_csv(output_file, mode='a', header=True)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--db",
+                        dest="db_path",
+                        required=True,
+                        help="path to exome or genome sqlite db")
+
+    parser.add_argument("--type",
+                        dest="run_type",
+                        required=True,
+                        help="function to be run: analyze sample `--type analyze` or update db `--type update`")
+
+    parser.add_argument("--input",
+                        dest="input_file",
+                        required=False,
+                        help="tsv file to update the db with. Only used if `--type update`")
+
+    args = parser.parse_args()
+
+    return args
+
+
+with open('allele_balance_log.txt', 'w') as log_file:
+    if __name__ == "__main__":
+        """
+        Required Files
+        1. sample.pileup
+        2. sample.vcf
+        3. Path to DB, as sys.argv[1]
+        Output
+        1. ab_report.txt
+        2. allele_balance_log.txt
+        """
+        args = vars(get_args())
+        if args['run_type'] == 'update' and 'input_file' == None:
+            print('Error, an input file is required to update the database')
+            log_file.write('Error, an input file is required to update the database\n')
+            quit()
+        elif args['run_type'] == 'update':
+            da = DatabaseAccess(args['db_path'])
+            da.update_db_with_tsv(args['input_file'])
+            pass
+        elif args['run_type'] == 'analyze':
+            run_sample(args['db_path'])
+        else:
+            print('Invalid `--type`. To analyze a sample use `--type analyze` or to update the db `--type update`')
+            log_file.write('Invalid `--type`. To analyze a sample use `--type analyze` or to update the db `--type update`\n')
+            quit()
